3ac57ad14b0237aadb8702beee1c256508e52d2f
Avetisian_3-027 <204596486+Avetisian-3-027@users.noreply.github.com>
2025-04-24 19:26:05 +0200

    LAB14

---
+++ LAB14/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void ProcessFile(string fileName)
+    {
+        int errorCount = 0;
+
+        try
+        {
+            var lines = File.ReadAllLines(fileName);
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("ERROR"))
+                {
+                    errorCount++;
+                }
+            }
+
+            Console.WriteLine($"Файл {fileName}: обнаружено {errorCount} ошибок.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при обработке файла {fileName}: {ex.Message}");
+        }
+    }
+    static void Ex1()
+    {
+        string[] fileNames = { "Something.txt", "Something1.txt", "Something2.txt" };
+
+        Task[] tasks = new Task[fileNames.Length];
+
+        for (int i = 0; i < fileNames.Length; i++)
+        {
+            int fileIndex = i;
+            tasks[i] = Task.Run(() => ProcessFile(fileNames[fileIndex]));
+        }
+
+        Task.WhenAll(tasks).Wait();
+
+        Console.WriteLine("Обработка файлов завершена!");
+    }
+    static void Main()
+    { 
+        Console.WriteLine("Ex1\n");
+        Ex1();
+    }
+}
